523688324aa30edfbc7f7549631893b09d72798a
nana <comfortappiah.ca@gmail.com>
2022-04-21 20:05:35 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,53 @@
+// document.write("it's a dog")
+// document.write(34*677);
+// console.log(true)
+
+var age = 101;
+var area = "100"
+console.log(age >= area)
+//console.log(age)
+let score = 300
+let mzbel = "i am 16 years old "
+let me = "i am goona be like this"
+console.log (mzbel  + me +" if you touch my thing" +" i go tell mummy oo")
+
+document.write( age +"<br>")
+document.write(score +"<br>")
+document.write(mzbel +"<br>")
+document.write(me +"<br>")
+
+
+
+
+
+
+
+
+
+
+
+// console.log(34 -45 + 56/67)
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(34 +45 /4)
+// console.log(23.4* 67)
+
+// var student = "dela";
+// var footBallClub = "psg";
+// var ghanaFood = "fufu";
+// var age = 50;
+// let country = "congo";
+// const number = 244322388;
+// student= "beatrice"
+// console.log(student)
